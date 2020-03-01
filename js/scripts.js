808ee54bc9e936c01badaea6b03afdcb2d1e82d1
postolenko <denis.postolenko@gmail.com>
2020-03-01 18:03:20 +0200

    Progame

--- js/scripts.js
+++ js/scripts.js
@@ -25,32 +25,32 @@ $(document).scroll(function() {
 
 $(document).ready(function() {
 
-    if( $(".portfolio_slider").length > 0 ) {
-        $(".portfolio_slider").not(".slick-initialized").slick({
-            dots: true,
+    if( $(".gallery").length > 0 ) {
+        $(".gallery").not(".slick-initialized").slick({
+            dots: false,
             arrows: true,
-            autoplay: true,
-            autoplaySpeed: 4000,
+            // autoplay: true,
+            // autoplaySpeed: 4000,
             speed: 1200,
-            slidesToShow: 4,
+            slidesToShow: 5,
             slidesToScroll: 1,
             // fade: true,
-            responsive: [
-                {
-                  breakpoint: 900,
-                  settings: {
-                    slidesToShow: 2,
-                    slidesToScroll: 2
-                  }
-                },
-                {
-                  breakpoint: 540,
-                  settings: {
-                    slidesToShow: 1,
-                    slidesToScroll: 1
-                  }
-                }
-              ]
+            // responsive: [
+            //     {
+            //       breakpoint: 900,
+            //       settings: {
+            //         slidesToShow: 2,
+            //         slidesToScroll: 2
+            //       }
+            //     },
+            //     {
+            //       breakpoint: 540,
+            //       settings: {
+            //         slidesToShow: 1,
+            //         slidesToScroll: 1
+            //       }
+            //     }
+            //   ]
         });
     }
 
